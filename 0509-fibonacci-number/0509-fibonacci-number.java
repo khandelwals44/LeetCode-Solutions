3d25d3be0a03c9c6d328557ecdc54cb626ac3992
khandelwals44 <34897943+khandelwals44@users.noreply.github.com>
2023-04-22 01:07:20 +0530

    Time: 8 ms (44.63%), Space: 39.8 MB (24.61%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        //Using Recurssion to solve the problem
+        // calulcating the sum of each f(n) till we reach f(1);        
+        if(n<=1)
+            return n;        
+        return fib(n-1)+ fib(n-2);      
+    }
+}
+//T.C = O(2^n)
+//S.C = O(1);
+//better tc can be achieved by Dyanamic Programming.
