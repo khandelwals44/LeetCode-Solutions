14f6d875c393a2e672a1e303230c64ae8dc90c6f
Shivam Khandelwal <khandelwals44@yahoo.com>
2023-06-20 23:48:20 +0530

    Time: 1 ms (74.76%), Space: 44.8 MB (35.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,5 +1,7 @@
 class Solution {
      public void setZeroes(int[][] matrix) {
+         int m = matrix.length;
+         int n = matrix[0].length;
 //         int index_i = 0;int index_j = 0;
 //         int[][] ans = new int[matrix.length][matrix[0].length];        
 //         for(int i = 0;i<matrix.length;i++){
@@ -40,29 +42,57 @@ class Solution {
 //             if(matrix[i][j]!=0)
 //                 matrix[i][j]=-1;
 //         }
-//     }    
-    int m= matrix.length, n= matrix[0].length;
-        int matrix2[][]= new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-                matrix2[i][j]=matrix[i][j];
-        }
+//     }  
+         
+         
+         
+         
+     //brute force
+//     int m= matrix.length, n= matrix[0].length;
+//         int matrix2[][]= new int[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++)
+//                 matrix2[i][j]=matrix[i][j];
+//         }
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++)
-                        matrix2[i][k]=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]==0){
+//                     for(int k=0;k<n;k++)
+//                         matrix2[i][k]=0;
 
-                    for(int k=0;k<m;k++)
-                        matrix2[k][j]=0;
+//                     for(int k=0;k<m;k++)
+//                         matrix2[k][j]=0;
+//                 }
+//             }
+//         }
+    
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++)
+//                 matrix[i][j]=matrix2[i][j];
+//         }
+         
+   
+        
+         //better
+         int[] row = new int[matrix.length];
+         int[] coloumn = new int[matrix[0].length];
+         Arrays. fill(row, 0);
+         Arrays. fill(coloumn, 0);
+         for(int i = 0;i<matrix.length;i++){
+            for(int j = 0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    coloumn[j]=1;
                 }
             }
         }
-    
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-                matrix[i][j]=matrix2[i][j];
+         for(int i = 0;i<matrix.length;i++){
+            for(int j = 0;j<matrix[0].length;j++){
+                if(row[i]==1 || coloumn[j] ==1){
+                    matrix[i][j]=0;
+                }
+            }
         }
 }
 }
